234146eb813c163bcc606f1473f96ce01bf17c63
Warren Santiago <wsantiago9623@gmail.com>
2022-10-04 23:01:56 -0400

    commit tree 160451635f94e0cd83f05160e1e089b3d241b502

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solutions in this file
+
+function writeCards(){
+
+    const nombres = [
+        "Guadalupe",
+        "Ollie", 
+        "Aki"
+    ];
+
+    let result = [];
+
+    nombres.map((nombre) => {
+        result.push("Thank you, " + nombre + ", for the wonderful surprise gift!");
+    })
+    
+    return result;
+}
+
+function countDown(number){
+    while(number >= 0) {
+        console.log(number);
+        number--;
+    }
+
+
+}
+
+
